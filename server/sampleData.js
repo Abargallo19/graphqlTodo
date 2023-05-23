fcd83a74e9b55e2292fa8ff9305b8f2b82e67b22
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-05-23 18:56:16 -0400

    import sample data into schema file

---
+++ server/sampleData.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        "id": "1",
+        "title": "Take out the trash",
+        "completed": true,
+    },
+    {
+        "id": "2",
+        "title": "Buy groceries",
+        "completed": true,
+    },
+    {
+        "id": "3",
+        "title": "Do laundry",
+        "completed": true,
+    },
+    {
+        "id": "4",
+        "title": "Pay bills",
+        "completed": true,
+    },
+]
+
+module.exports = { todos };
