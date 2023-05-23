ba62de6db828967a7436dee494f4b2e139176494
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-05-23 17:06:28 -0400

    console fully functional code thus far

---
+++ server/index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = 800;
+app.listen(PORT, console.log(`Locked & loaded on port ${PORT}`));
+console.log("Fully functional thus far")
