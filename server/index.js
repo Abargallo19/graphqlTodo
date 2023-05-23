15e1b5464b8f01d242269e113cdb026e419e6dfb
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-05-23 17:50:57 -0400

    required graphqlhttp within index.js file; no breaks

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+
+const { graphqlHTTP } = require('express-graphql');
+
 const app = express();
 const port = process.env.PORT || 8000;
-app.listen(port, console.log(`Locked & loaded on port ${port}`));
 
+
+app.use('/graphql', graphqlHTTP({}));
+app.listen(port, console.log(`Locked & loaded on port ${port}`));
+console.log("Checking for breaks")
