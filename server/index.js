a72cd188bab4996c31a4a17228263e86646dfe45
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2023-05-23 18:09:58 -0400

    creation of schema file

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 require('dotenv').config();
 
 const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+    schema, 
+    graphql: process.env.NODE_ENV === 'development'
+}));
 app.listen(port, console.log(`Locked & loaded on port ${port}`));
 console.log("Checking for breaks")
